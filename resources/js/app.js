3a2b226c631da6fb58e4c1deecdadc913bbca5aa
eren karaarslan <73473177+karaarslaneren@users.noreply.github.com>
2022-12-09 12:00:44 +0300

    fix proje

---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+import './bootstrap';
+
+import '../sass/app.scss'
+
+import * as bootstrap from 'bootstrap'
+
+import Alpine from 'alpinejs';
+window.Alpine = Alpine;
+import jQuery from 'jquery';
+window.$ = jQuery;
+
+Alpine.start();
